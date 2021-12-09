0530b5daff915dc4888fa812a30769e45efc4781
Мария <1481047@mail.ru>
2021-12-09 10:52:46 +0300

    Добавьте файлы проекта.

---
+++ VS18.1.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS18._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести строку со скобками");
+            string bracket = Console.ReadLine();
+            Console.WriteLine(Bracket(bracket) ? "Введено верно" : "Введено не верно");
+            Console.ReadKey();
+        }
+        static bool Bracket(string bracket)
+        {
+            string s = "[{(]})";
+            Stack<char> sb = new Stack<char>();
+            foreach (var i in bracket)
+            {
+                int b = s.IndexOf(i);
+
+                if (b >= 0 && b <= 2)
+                    sb.Push(i);
+
+                if (b > 2)
+                {
+                    if (sb.Count == 0 || sb.Pop() != s[b - 3])
+                        return false;
+                }
+            }
+            if (sb.Count != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
